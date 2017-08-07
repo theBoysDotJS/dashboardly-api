e947f076011bee7e42ea95bab1c564d35fd13731
Quinn Langille <quinn@quinnlangille.com>
2017-08-07 12:38:51 -0400

    setup bookmarks dataloader

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -7,15 +7,27 @@ module.exports = (dataLoader) => {
 
   // Modify a bookmark
   bookmarksController.patch('/:id', onlyLoggedIn, (req, res) => {
-    // TODO: this is up to you to implement :)
-    res.status(500).json({ error: 'not implemented' });
-  });
 
+    dataLoader.bookmarkBelongsToUser(req.params.id, req.user.id) // need to create this function
+    .then(() => {
+      return dataLoader.updateBookmark(req.params.id, { //need to create this function
+        title: req.body.title,
+        description: req.body.description
+      });
+    })
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json(err));
+  });
 
   // Delete a bookmark
   bookmarksController.delete('/:id', onlyLoggedIn, (req, res) => {
-    // TODO: this is up to you to implement :)
-    res.status(500).json({ error: 'not implemented' });
+
+    dataLoader.bookmarkBelongsToUser(req.params.id, req.user.id) // need to create function
+    .then(() => {
+      return dataLoader.deleteBoard(req.params.id); //need to create function
+    })
+    .then(() => res.status(204).end())
+    .catch(err => res.status(400).json(err));
   });
 
   return bookmarksController;
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use('/bookmarks', bookmarksController(dataLoader));
 
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   if (process.env.C9_HOSTNAME) {
     console.log(`Web server is listening on https://${process.env.C9_HOSTNAME}`);
